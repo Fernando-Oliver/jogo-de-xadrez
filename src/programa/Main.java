548db3b36be3c745e2355a9de9f8a13bee529100
Fernando de Oliveira Carvalho <fernandooliveiracarvalho98@gmail.com>
2022-04-30 10:50:14 -0300

    primeira classe: posição

--- src/programa/Main.java
+++ src/programa/Main.java
@@ -1,9 +1,12 @@
 package programa;
 
+import tabuleiro.Posicao;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		Posicao po = new Posicao(3,5);
+		System.out.println(po);
 	}
 
 }
